8c999d0bb6772b7121575c56c7dd2d90e9f01d27
User <User@SALA5>
2023-03-17 08:30:14 -0600

    Agregar archivos de proyecto.

---
+++ FACPYA.SistemaLibreria.Datos/bdContexto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace FACPYA.SistemaLibreria.Datos
+{
+    public class bdContexto
+    {
+        // Solo cadena de conexion, si se necesita ver detalles, editar o agregar una cadena de conexion nueva, ir al archivo
+        // Web.config de FACPYA.SIGA.Presentacion
+
+        // Nombre del ConnectionStrings a libre eleccion, si se desea cambiar afectar tambien al archivo web.config de Presentacion
+
+        static private readonly string appConexion = ConfigurationManager.ConnectionStrings["bdConexion"].ConnectionString;
+
+        // Se ejecuta stored con paramaetros
+        public static DataTable funcionStored(string sentencia, SqlParameter[] parametroSQL)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (var conn = new SqlConnection(appConexion))
+                {
+                    conn.Open();
+                    using (var comando = new SqlCommand(sentencia, conn))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+                        comando.Parameters.AddRange(parametroSQL);
+                        using (var adaptador = new SqlDataAdapter(comando))
+                        {
+                            adaptador.Fill(dt);
+                        }
+                    }
+                }
+                return dt;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        // Se ejecuta stored sin parametros
+        public static DataTable funcionStored(string sentencia)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (var conn = new SqlConnection(appConexion))
+                {
+                    conn.Open();
+                    using (var comando = new SqlCommand(sentencia, conn))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+                        using (var adaptador = new SqlDataAdapter(comando))
+                        {
+                            adaptador.Fill(dt);
+                        }
+                    }
+                }
+                return dt;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
